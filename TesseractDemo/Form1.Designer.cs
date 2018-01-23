f12e08ecad8680ab91ca14b6062bd200ab853462
Ayman Ali <aliayman@alumni.harvard.edu>
2018-01-22 16:57:27 -0800

    Initial files

---
+++ TesseractDemo/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace TesseractDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnDownloadWTU = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.Olive;
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.btnDownloadWTU);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(684, 0);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(8, 3, 3, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(132, 458);
+            this.flowLayoutPanel1.TabIndex = 1;
+            this.flowLayoutPanel1.WrapContents = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(661, 182);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Run Tesseract";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(12, 358);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(661, 70);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnDownloadWTU
+            // 
+            this.btnDownloadWTU.Location = new System.Drawing.Point(3, 61);
+            this.btnDownloadWTU.Name = "btnDownloadWTU";
+            this.btnDownloadWTU.Size = new System.Drawing.Size(117, 23);
+            this.btnDownloadWTU.TabIndex = 2;
+            this.btnDownloadWTU.Text = "Get Image from WTU";
+            this.btnDownloadWTU.UseVisualStyleBackColor = true;
+            this.btnDownloadWTU.Click += new System.EventHandler(this.btnDownloadWTU_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 458);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnDownloadWTU;
+    }
+}
+
---
+++ TesseractDemo/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+using Tesseract;
+
+namespace TesseractDemo
+{
+    public partial class Form1 : Form
+    {
+        //http://www.waytoumrah.com/prj_umrah/include/image.aspx?imgtext=538
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var fd = new OpenFileDialog();
+            var result = fd.ShowDialog();
+            if (result == DialogResult.OK && !string.IsNullOrEmpty(fd.FileName))
+                pictureBox1.ImageLocation = fd.FileName;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = RunTesseract(new Bitmap(pictureBox1.Image));
+        }
+
+
+        private string RunTesseract(Bitmap inputImage)
+        {
+            var res = "";
+            using (var engine = new TesseractEngine(@"../../tessdata", "eng", EngineMode.Default))
+            {
+                //engine.SetVariable("tessedit_char_whitelist", "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+                engine.SetVariable("tessedit_char_whitelist", "1234567890");
+                engine.SetVariable("tessedit_unrej_any_wd", true);
+
+                using (var page = engine.Process(inputImage, PageSegMode.SingleLine))
+                {
+                    res = page.GetText();
+                }
+            }
+            return res;
+        }
+
+        private void btnDownloadWTU_Click(object sender, EventArgs e)
+        {
+            var wc = new WebClient();
+            var result = wc.DownloadData("http://www.waytoumrah.com/prj_umrah/include/image.aspx?imgtext=538");
+
+            Bitmap bmp;
+            using (var ms = new MemoryStream(result))
+            {
+                bmp = new Bitmap(ms);
+            }
+
+            pictureBox1.Image = bmp;
+        }
+    }
+}
